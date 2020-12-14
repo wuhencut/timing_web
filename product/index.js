002211c99dff2d84a6da5b61183cbb64263931f4
stone <stone_rivers@163.com>
2020-12-14 19:17:10 +0800

    autoplay 产品index页面完成

--- product/index.js
+++ product/index.js
@@ -1,17 +1,38 @@
 new Vue({
-  el: "#app",
+  el: "#page",
   data() {
-    return {}
+    return {
+      active1: 1,
+      active2: 1, // nav 
+    }
   },
   mounted() {
     this.initSwiper1();
   },
   methods: {
+    click2(num) {
+      if(num != this.active2){
+        this.active2 = num;
+      } else {
+        this.active2 = 3;
+      }
+    },
+    click1(num) {
+      this.active1 = num;
+    },
     initSwiper1() {
       new Swiper(".swiper1", {
         loop: true,
         pagination: {
           el: '.swiper-pagination',
+        },
+        speed: 2000,
+        autoplay: {
+          delay: 5000,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
         },
       })
     }
